c145aa1e5853a20866ab0f5912e9452ccb5bc831
Anton <99226421+AntonR-github@users.noreply.github.com>
2023-06-29 00:23:44 +0300

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,30 @@
+import Navbar from './Navbar';
+import Hero from './Hero';
+import Clients from './Clients';
+import Footer from './Footer';
+import Billing from './Billing';
+import Business from './Business';
+import Button from './Button';
+import CardDeal from './CardDeal';
+import CTA from './CTA';
+import FeedbackCard from './FeedbackCard';
+import GetStarted from './GetStarted';
+import Stats from './Stats';
+import Testimonials from './Testimonials';
+
+export {
+    Navbar,
+    Hero,
+    Clients,
+    Footer,
+    Billing,
+    Business,
+    Button,
+    CardDeal,
+    CTA,
+    FeedbackCard,
+    GetStarted,
+    Stats,
+    Testimonials
+};
+
